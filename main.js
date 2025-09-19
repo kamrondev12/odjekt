3e3a1df93a5d3f0a61c3d06edde544472113a256
kamrondev12 <abuabdullaev91@gmail.com>
2025-09-19 15:52:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+ const ism = prompt('ismingizni kriting'),
+  id = prompt('Space id kiriting'),
+  parol = prompt("parolingizni kiriting")
+
+  const obj =  {
+   ism,id , parol  }
+
+
+ console.log( obj);
+ 
